a80c0dfc65b4ddd657c074a71dcc18b08abad0a1
Vishwanathhiremath112 <vishwanathhiremath131@gmail.com>
2022-09-12 18:14:29 +0530

    added changes

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using MVCDemo.Data;
+using ContosoUniversity.Data;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<MVCDemoContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCDemoContext") ?? throw new InvalidOperationException("Connection string 'MVCDemoContext' not found.")));
+builder.Services.AddDbContext<ContosoUniversityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ContosoUniversityContext") ?? throw new InvalidOperationException("Connection string 'ContosoUniversityContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
